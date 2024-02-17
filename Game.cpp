b49b00131aa597bfd79eb67a1acba3dc5b029380
Flu3nt <gnedyalkov94@gmail.com>
2024-02-17 10:24:00 +0200

    add a SDL renderer

--- Game.cpp
+++ Game.cpp
@@ -2,19 +2,29 @@
 
 bool Game::Initialize()
 {
+    // Initialize SDL
     int sdlResult = SDL_Init(SDL_INIT_VIDEO);
-
     if (sdlResult != 0)
     {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return false;
     }
 
+    // Create the window
     mWindow = SDL_CreateWindow("Game Programming in C++ Chapter 1", 100, 100, 1024, 768, 0);
-
     if (!mWindow)
+
     {
         SDL_Log("Failed to create window: %s", SDL_GetError());
+        return false;
+    }
+
+    // Create the renderer
+    mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (!mRenderer)
+
+    {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
         return false;
     }
 
@@ -31,8 +41,30 @@ void Game::RunLoop()
 {
     while (mIsRunning)
     {
-        // ProceesInput();
+        ProcessInput();
         // UpdateGame();
         // GenerateOutput();
+    }
+}
+
+void Game::ProcessInput()
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            mIsRunning = false;
+            break;
+        }
+    }
+
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+
+    if (state[SDL_SCANCODE_ESCAPE])
+    {
+        mIsRunning = false;
     }
 }
