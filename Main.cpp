f6519dd4c3ff7459d6f58e325b9468345e3b0add
Flu3nt <gnedyalkov94@gmail.com>
2024-02-17 10:04:48 +0200

    setup the game initalization

--- Main.cpp
+++ Main.cpp
@@ -1,35 +1,13 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-
-const int WIDTH = 800, HEIGHT = 600;
+#include "Game.h"
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window *window = SDL_CreateWindow("SDL Template", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-
-    if (NULL == window)
+    Game game;
+    bool success = game.Initialize();
+    if (success)
     {
-        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-        return 1;
+        game.RunLoop();
     }
-
-    SDL_Event windowEvent;
-
-    while (true)
-    {
-        if (SDL_PollEvent(&windowEvent))
-        {
-            if (SDL_QUIT == windowEvent.type)
-            {
-                break;
-            }
-        }
-    }
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
+    game.Shutdown();
+    return 0;
 }
